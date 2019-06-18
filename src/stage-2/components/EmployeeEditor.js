9567628ca4e8dabc371ea6d8ab003c4add8c38fa
John Paulo Barbin <john.barbin@boom.camp>
2019-06-18 18:05:34 +0800

    Done Stage 1 to 9

--- src/stage-2/components/EmployeeEditor.js
+++ src/stage-2/components/EmployeeEditor.js
@@ -8,9 +8,24 @@ class EmployeeEditor extends Component {
       originalEmployee: null,
       notModified: true,
     };
+
+    this.save = this.save.bind(this);
+    this.cancel = this.cancel.bind(this);
   }
 
   // componentWillReceiveProps
+  componentDidUpdate(prevProps) {
+
+    // checking this condition so we don't update unless there was a change.
+    // if we don't do this we could cause an infinite loop.
+    if (prevProps.selected !== this.props.selected) {
+      this.setState({
+        employee: Object.assign({}, this.props.selected),
+        originalEmployee: this.props.selected,
+        notModified: true
+        });
+    }
+  }
 
   handleChange(prop, val) {
     if (this.state.notModified) {
