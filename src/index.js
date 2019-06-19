4f72baf5c2ff1121bf439838c3c16dd9341c8fe1
Alan John Alilano <alan.alilano@boom.camp>
2019-06-19 14:53:08 +0800

    Stage 8 Done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './stage-7/App';
+import App from './stage-8/App';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
