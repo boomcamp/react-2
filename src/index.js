4d08b62eb587a5d0b9021bdbedd1de95ee8d8029
Martin Raquion <raquionearl@gmail.com>
2019-06-19 10:09:48 +0800

    finsihed stage 3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './stage-2/App';
+import App from './stage-3/App';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
