cd4fa794960a5d2cf7e354328bf3fb51e53e499e
Martin Raquion <raquionearl@gmail.com>
2019-06-19 13:12:52 +0800

    modified stage 3

--- src/stage-3/components/EmployeeEditor.js
+++ src/stage-3/components/EmployeeEditor.js
@@ -16,9 +16,6 @@ class EmployeeEditor extends Component {
   // componentWillReceiveProps
 
   componentDidUpdate(prevProps) {
-
-    // checking this condition so we don't update unless there was a change.
-    // if we don't do this we could cause an infinite loop.
     if (prevProps.selected !== this.props.selected) {
       this.setState({
         employee: Object.assign({}, this.props.selected),
