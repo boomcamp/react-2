7f33b48fb696170e0cdda859037c6de6deb5734d
Vincent B. Navas <vhincebotalon@gmail.com>
2019-06-19 15:03:25 +0800

    submission

--- src/stage-6/components/EmployeeEditor.js
+++ src/stage-6/components/EmployeeEditor.js
@@ -11,12 +11,41 @@ class EmployeeEditor extends Component {
   }
 
   // componentWillReceiveProps
+  componentDidUpdate(prevProps) {
+    if (prevProps.selected !== this.props.selected) {
+      this.setState({
+        employee: Object.assign({}, this.props.selected),
+        originalEmployee: this.props.selected,
+        notModified: true,
+      });
+    }
+  }
 
   // handleChange
+  handleChange(prop, val) {
+    if (this.state.notModified) {
+      this.setState({ notModified: false });
+    }
+
+    var employeeCopy = Object.assign({}, this.state.employee);
+    employeeCopy[prop] = val;
+    this.setState({ employee: employeeCopy });
+  }
 
   // save
+  save() {
+    this.state.originalEmployee.updateName(this.state.employee.name);
+    this.state.originalEmployee.updatePhone(this.state.employee.phone);
+    this.state.originalEmployee.updateTitle(this.state.employee.title);
+    this.setState({ notModified: true });
+    this.props.refreshList();
+  }
 
   // cancel
+  cancel() {
+    var employeeCopy = Object.assign({}, this.state.originalEmployee);
+    this.setState({ employee: employeeCopy, notModified: true });
+  }
 
   render() {
     return (
