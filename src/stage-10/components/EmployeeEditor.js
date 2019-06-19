b6ce0cedcd6295fff33a55efbc5de75eb4010185
marvinbanton <marvin.banton@boom.camp>
2019-06-19 09:08:15 +0800

    submission

--- src/stage-10/components/EmployeeEditor.js
+++ src/stage-10/components/EmployeeEditor.js
@@ -1,48 +1,52 @@
 import React, { Component } from 'react';
 
 class EmployeeEditor extends Component {
-  constructor() {
-    super();
-    this.state = {
-      employee: null,
-      originalEmployee: null,
-      notModified: true,
-    };
-    this.save = this.save.bind(this);
-    this.cancel = this.cancel.bind(this);
-  }
+    constructor() {
+        super ();
+        this.state ={
+          employee: null,
+          originalEmployee: null,
+          notModified: true,
+        }
+        this.save = this.save.bind(this);
+        this.cancel = this.cancel.bind(this);
+      }
 
-  componentWillReceiveProps(props) {
-    this.setState({
-      employee: Object.assign({}, props.selected),
-      originalEmployee: props.selected,
-    });
-  }
-
-  handleChange(prop, val) {
-    if (this.state.notModified) {
-      this.setState({ notModified: false });
+    componentDidUpdate(prevProps){
+     
+        if (prevProps.selected !== this.props.selected) {
+          this.setState({
+            employee: Object.assign({}, this.props.selected),
+            originalEmployee: this.props.selected,
+            notModified: true
+            });
+        }
     }
 
-    var employeeCopy = Object.assign({}, this.state.employee);
-    employeeCopy[prop] = val;
-    this.setState({ employee: employeeCopy });
-  }
+    handleChange(propName, val) {
+        if (this.state.notModified) {
+          this.setState({ notModified: false });
+        }
 
-  save() {
-    this.state.originalEmployee.updateName(this.state.employee.name);
-    this.state.originalEmployee.updatePhone(this.state.employee.phone);
-    this.state.originalEmployee.updateTitle(this.state.employee.title);
-    this.setState({ notModified: true });
-    this.props.refreshList();
-  }
+        var employeeCopy = Object.assign({}, this.state.employee);
+        employeeCopy[propName] = val;
+        this.setState({ employee: employeeCopy });
+    }
 
-  cancel() {
-    var employeeCopy = Object.assign({}, this.state.originalEmployee);
-    this.setState({ employee: employeeCopy, notModified: true });
-  }
+    save() {
+        this.state.originalEmployee.updateName(this.state.employee.name);
+        this.state.originalEmployee.updatePhone(this.state.employee.phone);
+        this.state.originalEmployee.updateTitle(this.state.employee.title);
+        this.setState({ notModified: true });
+        this.props.refreshList();
+    }
 
-  render() {
+    cancel() {
+        var employeeCopy = Object.assign({}, this.state.originalEmployee);
+        this.setState({ employee: employeeCopy, notModified: true });
+    }
+
+render() {
     return (
       <div className="infoCard">
         {this.state.employee ? (
