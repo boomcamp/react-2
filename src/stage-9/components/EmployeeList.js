1ef4ca385311827ea4d9d934f0428a64625fae41
Martha Dansyle Marbella <martha.marbella@boom.camp>
2019-11-12 10:44:08 +0800

    Stage 9 - Done

--- src/stage-9/components/EmployeeList.js
+++ src/stage-9/components/EmployeeList.js
@@ -1,17 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class EmployeeList extends Component {
-  render() {
-    return (
-      <div>
-        <ul className="listContainer">
-          {
-            // Map over this.props.employees
-          }
-        </ul>
-      </div>
-    );
-  }
+   render() {
+      return (
+         <div>
+            <ul className="listContainer">
+               {this.props.employees.map(employee => {
+                  return (
+                     <li
+                        className="listText"
+                        key={employee.id}
+                        onClick={() => {
+                           this.props.selectEmployee(employee);
+                        }}
+                     >
+                        {" "}
+                        {employee.name}{" "}
+                     </li>
+                  );
+               })}
+            </ul>
+         </div>
+      );
+   }
 }
 
 export default EmployeeList;
