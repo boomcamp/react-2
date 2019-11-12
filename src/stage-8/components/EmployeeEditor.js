d7b016bdc32a2f561c2ba23442c9d07f8d2a6d3c
JaymarMenor <jaymardmenor001@gmail.com>
2019-11-12 17:40:50 +0800

    submission

--- src/stage-8/components/EmployeeEditor.js
+++ src/stage-8/components/EmployeeEditor.js
@@ -2,14 +2,51 @@ import React, { Component } from 'react';
 
 class EmployeeEditor extends Component {
   // constructor
+    constructor(){
+      super();
+      this.state={
+        employee: null,
+        originalEmployee: null,
+        notModified: true,
+      }
+      this.save= this.save.bind(this);
+      this.cancel = this.cancel.bind(this);
+    }
+  // componentDidUpdate
+  componentDidUpdate(prevProps){
+  if(prevProps.selected !== this.props.selected){
+    this.setState=({
+      employee: Object.assign({}, this.props.selected),
+      originalEmployee: props.selected,
+      notModified: true,
 
-  // componentWillReceiveProps
+    })
+  }
+
+  }
 
   // handleChange
+    handleChange(propName, val){
+      if(this.state.notModified){
+        this.setState({notModified:false})
+      }
 
+    }
   // save
-
+    save(){
+      this.state.originalEmployee.updateName(this.state.employee.name);
+      this.state.originalEmployee.updatePhone(this.state.employee.phone);
+      this.state.originalEmployee.updateTitle(this.state.employee.title);
+      this.setState({notModified: true});
+      this.props.refreshList();
+    }
   // cancel
+    cancel(){
+      this.setState({
+        employee: Object.assign({}, this.props.selected),
+        notModified: true
+      })
+    }
 
   render() {
     return (
