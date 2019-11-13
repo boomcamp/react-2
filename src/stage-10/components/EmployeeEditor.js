e4d9727eb24eb2a80842cfa9a66057a254f9bfc6
serra-vp <vincent.serra@boom.camp>
2019-11-13 11:48:07 +0800

    Completed react-2

--- src/stage-10/components/EmployeeEditor.js
+++ src/stage-10/components/EmployeeEditor.js
@@ -1,46 +1,56 @@
 import React, { Component } from 'react';
 
 class EmployeeEditor extends Component {
-  constructor() {
+  constructor(){
     super();
     this.state = {
       employee: null,
       originalEmployee: null,
-      notModified: true,
-    };
-
+      notModified: true
+    }
     this.save = this.save.bind(this);
     this.cancel = this.cancel.bind(this);
   }
 
-  componentWillReceiveProps(props) {
-    this.setState({
-      employee: Object.assign({}, props.selected),
-      originalEmployee: props.selected,
-    });
+  componentDidUpdate(prevProps) {
+    if (prevProps.selected !== this.props.selected) {
+      this.setState({
+        employee: Object.assign({}, this.props.selected),
+        originalEmployee: this.props.selected,
+        notModified: true,
+      })
+    }
   }
 
-  handleChange(prop, val) {
-    if (this.state.notModified) {
-      this.setState({ notModified: false });
+  handleChange(propName, val){
+    if (this.state.notModified){
+      this.setState({ 
+        notModified: false
+      });
     }
 
     var employeeCopy = Object.assign({}, this.state.employee);
-    employeeCopy[prop] = val;
-    this.setState({ employee: employeeCopy });
+    employeeCopy[propName] = val;
+    this.setState({ 
+      employee: employeeCopy
+    });
   }
-
-  save() {
+  
+  save(){
     this.state.originalEmployee.updateName(this.state.employee.name);
     this.state.originalEmployee.updatePhone(this.state.employee.phone);
     this.state.originalEmployee.updateTitle(this.state.employee.title);
-    this.setState({ notModified: true });
+    this.setState({
+      notModified: true
+    });
     this.props.refreshList();
   }
 
-  cancel() {
-    var employeeCopy = Object.assign({}, this.state.originalEmployee);
-    this.setState({ employee: employeeCopy, notModified: true });
+  cancel(){
+    this.setState({
+      employee: Object.assign({}, this.props.selected),
+      notModified: true
+    })
   }
 
   render() {
