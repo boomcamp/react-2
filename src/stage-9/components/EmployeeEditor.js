ac83156c8077e379328c0291f0bd344091eabf3a
Christian Ray Frecia <christian.frecia@boom.camp>
2019-11-13 11:58:26 +0800

    submitted

--- src/stage-9/components/EmployeeEditor.js
+++ src/stage-9/components/EmployeeEditor.js
@@ -1,15 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class EmployeeEditor extends Component {
   // constructor
+  constructor() {
+    super();
+    this.state = {
+      employee: null,
+      originalEmployee: null,
+      notModified: true
+    };
+    this.save = this.save.bind(this)
+    this.cancel = this.cancel.bind(this)
+  }
 
   // componentWillReceiveProps
-
+  componentWillReceiveProps(props){
+    this.setState({
+      employee: Object.assign({}, props.selected),
+      originalEmployee: props.selected,
+    })
+  }
   // handleChange
-
+  handleChange(propName, val){
+    if(this.state.notModified){
+      this.setState({ notModified: false });
+    }
+    var employeeCopy = Object.assign({}, this.state.employee)
+    employeeCopy[propName] = val;
+    this.setState({ employee: employeeCopy })
+  }
   // save
+  save(){
+    this.state.originalEmployee.updateName(this.state.employee.name);
+    this.state.originalEmployee.updatePhone(this.state.employee.phone);
+    this.state.originalEmployee.updateTitle(this.state.employee.title);
+    this.setState({ notModified: true })
+    this.props.refreshList();
+  }
 
   // cancel
+  cancel(){
+    var employeeCopy = Object.assign({}, this.state.originalEmployee);
+    this.setState({ employee: employeeCopy, notModified: true })
+  }
 
   render() {
     return (
@@ -25,16 +58,16 @@ class EmployeeEditor extends Component {
               disabled={this.state.notModified}
               onClick={this.save}
             >
-              {' '}
-              Save{' '}
+              {" "}
+              Save{" "}
             </button>
             <button
               className="neutralButton"
               disabled={this.state.notModified}
               onClick={this.cancel}
             >
-              {' '}
-              Cancel{' '}
+              {" "}
+              Cancel{" "}
             </button>
             <br />
             <span className="placeholderText"> Name </span>
@@ -42,7 +75,7 @@ class EmployeeEditor extends Component {
               className="materialInput"
               value={this.state.employee.name}
               onChange={e => {
-                this.handleChange('name', e.target.value);
+                this.handleChange("name", e.target.value);
               }}
             />
             <span className="placeholderText"> Phone Number </span>
@@ -50,7 +83,7 @@ class EmployeeEditor extends Component {
               className="materialInput"
               value={this.state.employee.phone}
               onChange={e => {
-                this.handleChange('phone', e.target.value);
+                this.handleChange("phone", e.target.value);
               }}
             />
             <span className="placeholderText"> Title </span>
@@ -58,7 +91,7 @@ class EmployeeEditor extends Component {
               className="materialInput"
               value={this.state.employee.title}
               onChange={e => {
-                this.handleChange('title', e.target.value);
+                this.handleChange("title", e.target.value);
               }}
             />
           </div>
